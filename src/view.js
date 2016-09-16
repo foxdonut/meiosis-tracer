4451bfa6bfb30fce50cdb036e5842ce3847a81c4
foxdonut <fdaoud@fastmail.com>
2016-09-16 19:12:50 -0400

    Add Show/Hide button to tracer.

--- src/view.js
+++ src/view.js
@@ -5,7 +5,9 @@ const jsonFormatConfig = {
   size: 2
 };
 
+const tracerContainerId = "tracerContainer";
 const tracerId = "tracerSlider";
+const tracerToggleId = "tracerToggle";
 const tracerIndexId = "tracerIndex";
 const tracerModelId = "tracerModel";
 const tracerProposalId = "tracerProposal";
@@ -43,11 +45,25 @@ const onModelChange = renderRoot => evt => {
   }
 };
 
+const onToggle = tracerContainer => evt => {
+  const button = evt.target;
+
+  if (tracerContainer.style.display === "none") {
+    tracerContainer.style.display = "block";
+    button.innerHTML = "Hide";
+  }
+  else {
+    tracerContainer.style.display = "none";
+    button.innerHTML = "Show";
+  }
+};
+
 const initialView = (selector, renderRoot, tracerModel) => {
   const target = document.querySelector(selector);
 
   if (target) {
-    const viewHtml = "<div><input id='" + tracerId + "' type='range' min='0' max='" +
+    const viewHtml = "<div style='text-align: right'><button id='" + tracerToggleId + "'>Hide</button></div>" +
+      "<div id='" + tracerContainerId + "'><input id='" + tracerId + "' type='range' min='0' max='" +
       String(tracerModel.tracerStates.length - 1) +
       "' value='" + String(tracerModel.tracerIndex) + "' style='width: 100%'/>" +
       "<div id='" + tracerIndexId + "'>" + String(tracerModel.tracerIndex) + "</div>" +
@@ -55,8 +71,12 @@ const initialView = (selector, renderRoot, tracerModel) => {
       "<textarea id='" + tracerModelId + "' rows='20' cols='40' style='display: block'></textarea></div>";
 
     target.innerHTML = viewHtml;
+
+    const tracerContainer = document.getElementById(tracerContainerId);
+
     document.getElementById(tracerId).addEventListener("input", onSliderChange(renderRoot, tracerModel));
     document.getElementById(tracerModelId).addEventListener("keyup", onModelChange(renderRoot));
+    document.getElementById(tracerToggleId).addEventListener("click", onToggle(tracerContainer));
   }
 };
 
