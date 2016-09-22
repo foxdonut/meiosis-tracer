a0562b409020080b685ce800b0e6955b2f2f10b4
foxdonut <fdaoud@fastmail.com>
2016-09-22 04:42:51 -0400

    Working on connecting devtools tracer to Meiosis app.

--- chrome-devtool/content-script.js
+++ chrome-devtool/content-script.js
@@ -11,32 +11,29 @@ document.documentElement.appendChild(script);
 script.parentNode.removeChild(script);
 
 var sendObjectToDevTools = function(message) {
-  console.log("injected send object to dev tools:", message);
   chrome.extension.sendMessage(message);
 };
 
 window.addEventListener("message", function(evt) {
-  console.log("injected received:", evt);
   /*
   if (evt.source != window) {
     return;
   }
   */
   if (evt.data.type === "MEIOSIS_RECEIVE") {
-    console.log("-- MEIOSIS_RECEIVE");
     sendObjectToDevTools({data: evt.data});
   }
   else if (evt.data.type === "MEIOSIS_INITIAL_MODEL") {
-    console.log("-- MEIOSIS_INITIAL_MODEL");
     sendObjectToDevTools({data: evt.data});
   }
 });
-console.log("injected added message listener.");
 
 chrome.runtime.onMessage.addListener(function(message) {
-  console.log("** injected rcvd message from extension:", message);
   if (message.content.type === "MEIOSIS_REQUEST_INITIAL_MODEL") {
     window.postMessage({ type: "MEIOSIS_REQUEST_INITIAL_MODEL" }, "*");
+  }
+  else if (message.content.type === "MEIOSIS_RENDER_ROOT") {
+    window.postMessage({ type: "MEIOSIS_RENDER_ROOT", model: message.content.model }, "*");
   }
 });
 
--- chrome-devtool/panel.js
+++ chrome-devtool/panel.js
@@ -10,61 +10,45 @@
 //   tabId: [Automatically added]
 // }
 (function() {
-  console.log("panel initialize");
-
   // This sends an object to the background page where it can be relayed to the inspected page
   var sendObjectToInspectedPage = function(message) {
-    console.log("< panel sending object to inspected page >");
     message.tabId = chrome.devtools.inspectedWindow.tabId;
     chrome.extension.sendMessage(message);
   };
 
   //Create a port with background page for continous message communication
-  console.log("panel setup port");
   var port = chrome.extension.connect({
     name: "Meiosis-Tracer Channel"
   });
 
+  var receive = null;
+
   // Listen to messages from the background page
-  console.log("panel listen");
   port.onMessage.addListener(function(evt) {
     var data = JSON.parse(evt).message.data;
     var model = data.model;
     var proposal = data.proposal;
 
     if (data.type === "MEIOSIS_INITIAL_MODEL") {
-      console.log("panel MEIOSIS_INITIAL_MODEL:", model);
+      var createComponent = function(config) {
+        receive = config.receive;
+      };
+
+      // To re-render the view, send a message.
+      var renderRoot = function(model) {
+        sendObjectToInspectedPage({ content: { type: "MEIOSIS_RENDER_ROOT", model: model } });
+      };
+      renderRoot.initialModel = model;
+
+      var selector = "#meiosis-tracer";
+
+      var viewProxy = null;
+
+      window.meiosisTracer(createComponent, renderRoot, selector, viewProxy);
     }
-    else if (data.type === "MEIOSIS_RECEIVE") {
-      console.log("panel MEIOSIS_RECEIVE:", model, proposal);
+    else if (data.type === "MEIOSIS_RECEIVE" && receive) {
+      receive(model, proposal);
     }
-    else {
-      console.log("panel received:", evt);
-    }
-    //document.querySelector("#insertmessagebutton").innerHTML = message.content;
   });
-  console.log("panel send request for initial model");
   sendObjectToInspectedPage({ content: { type: "MEIOSIS_REQUEST_INITIAL_MODEL" } });
-
-  // No need to create a component, since this is done at the Meiosis level.
-  var createComponent = function() {
-  };
-
-  // To re-render the view, send a message.
-  var renderRoot = function(model) {
-    var tabId = chrome.devtools.inspectedWindow.tabId;
-    chrome.extension.sendMessage({ tabId: tabId, content: { model: model } });
-  };
-
-  var selector = "#meiosis-tracer";
-
-  var viewProxy = null;
-
-  //window.meiosisTracer(createComponent, renderRoot, selector, viewProxy);
-
-/*
-  console.log("panel inject script");
-  sendObjectToInspectedPage({action: "script", content: "injected.js"});
-*/
-
 })();
