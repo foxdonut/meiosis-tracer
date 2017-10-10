fa07dfdab5ca06a2eb75afe961edd8136f5839c7
foxdonut <fdaoud@fastmail.com>
2017-10-10 19:45:28 -0400

    Fix #1

--- src/receive.js
+++ src/receive.js
@@ -1,5 +1,8 @@
 const createReceiveValues = (tracerModel, view) => (values, update) => {
   if (update) {
+    if (tracerModel.tracerStates.length > 0) {
+      tracerModel.tracerStates.length = tracerModel.tracerIndex + 1;
+    }
     tracerModel.tracerStates.push(values);
     tracerModel.tracerIndex = tracerModel.tracerStates.length - 1;
   }
